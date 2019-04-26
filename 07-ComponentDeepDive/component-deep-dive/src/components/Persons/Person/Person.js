1c66ce8d5bc44067a2d99e4bdb42b5ef6280b836
AnandDev.Singh <ananddev.singh@externals.hilti.com>
2019-04-26 16:29:13 +0530

    way 1- rendering adjecent JSX elements

--- 07-ComponentDeepDive/component-deep-dive/src/components/Persons/Person/Person.js
+++ 07-ComponentDeepDive/component-deep-dive/src/components/Persons/Person/Person.js
@@ -1,22 +1,25 @@
 import React, { Component } from "react";
 
-import classes from "./Person.css";
+// import classes from "./Person.css";
 
 class Person extends Component {
   render() {
     console.log("[Person.js] rendering...");
     return (
-      <div className={classes.Person}>
-        <p onClick={this.props.click}>
+      // <div className={classes.Person}>
+      [
+        <p key="key1" onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
-        </p>
-        <p>{this.props.children}</p>
+        </p>,
+        <p key="key2">{this.props.children}</p>,
         <input
+          key="key3"
           type="text"
           onChange={this.props.changed}
           value={this.props.name}
         />
-      </div>
+      ]
+      // </div>
     );
   }
 }
