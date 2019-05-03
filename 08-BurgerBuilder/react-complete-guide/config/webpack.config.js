fb86d2ca271d8320af589842e9da11c8314f037b
AnandDev.Singh <ananddev.singh@externals.hilti.com>
2019-05-04 01:29:14 +0530

    Added basic burger builder component and corrected css loaders

--- 08-BurgerBuilder/react-complete-guide/config/webpack.config.js
+++ 08-BurgerBuilder/react-complete-guide/config/webpack.config.js
@@ -402,6 +402,7 @@ module.exports = function(webpackEnv) {
               exclude: cssModuleRegex,
               use: getStyleLoaders({
                 importLoaders: 1,
+                modules: true,
                 localIdentName: "[name]__[local]__[hash:base64:5]",
                 sourceMap: isEnvProduction && shouldUseSourceMap
               }),
