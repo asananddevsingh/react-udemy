34fad67a1df1908a1862c4858e5baa1d7bc67ad8
AnandDev.Singh <ananddev.singh@externals.hilti.com>
2019-05-08 00:24:37 +0530

    162. Displaying and Updating the Burger Price

--- 08-BurgerBuilder/react-complete-guide/src/containers/BurgerBuilder/BurgerBuilder.js
+++ 08-BurgerBuilder/react-complete-guide/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -71,6 +71,7 @@ class BurgerBuilder extends React.Component {
           ingredientAdded={this.addIngriedientHandler}
           ingredientRemoved={this.removeIngriedientHandler}
           disabled={disabledInfo}
+          price={this.state.totalPrice}
         />
       </Aux>
     );
