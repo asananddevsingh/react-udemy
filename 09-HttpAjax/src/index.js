4e1621fd406837f70f91eeed604c87f984fb8947
AnandDev.Singh <ananddevsingh.aiet@gmail.com>
2019-05-17 15:15:24 +0530

    198. Adding Interceptors to Execute Code Globally

--- 09-HttpAjax/src/index.js
+++ 09-HttpAjax/src/index.js
@@ -1,8 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import axios from "axios";
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+// It will thwor error once the request is fail, such as no internet connection.
+axios.interceptors.request.use(
+  request => {
+    console.log("[index.js] request: ", request);
+    // You can edit the request config here.
+    return request;
+  },
+  error => {
+    console.log("[index.js] request error: ", error);
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  response => {
+    console.log("[index.js] response: ", response);
+    // You can edit the response config here.
+    return response;
+  },
+  error => {
+    console.log("[index.js] response error: ", error);
+    return Promise.reject(error);
+  }
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
