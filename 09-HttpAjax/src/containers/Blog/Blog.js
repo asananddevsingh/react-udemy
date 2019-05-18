d9729b3c867501748ae228439c3ea057bfc580c4
AnandDev.Singh <ananddevsingh.aiet@gmail.com>
2019-05-19 00:42:48 +0530

    221. Rendering Components for Routes

--- 09-HttpAjax/src/containers/Blog/Blog.js
+++ 09-HttpAjax/src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
 
 import "./Blog.css";
 import Posts from "./Posts/Posts";
@@ -19,7 +20,9 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <Posts />
+        <Route path="/" exact render={() => <h1>Home 1</h1>} />
+        <Route path="/" exact render={() => <h1>Home 2</h1>} />
+        {/* <Posts /> */}
       </div>
     );
   }
