cba1b5e2bb0fc5426cf9d3d0a4f33751f55c62ce
AnandDev.Singh <ananddevsingh.aiet@gmail.com>
2019-05-27 00:29:14 +0530

    266. Adding a Dropdown Component

--- 08-BurgerBuilder/react-complete-guide/src/components/UI/Input/Input.jsx
+++ 08-BurgerBuilder/react-complete-guide/src/components/UI/Input/Input.jsx
@@ -15,6 +15,19 @@ const input = props => {
         <textarea className={classes.InputElement} {...props.elementConfig} />
       );
       break;
+    case "select":
+      inputElement = (
+        <select className={classes.InputElement} value={props.value}>
+          {props.elementConfig.options.map(option => {
+            return (
+              <option key={option.value} value={option.value}>
+                {option.displayValue}
+              </option>
+            );
+          })}
+        </select>
+      );
+      break;
     default:
       inputElement = (
         <input className={classes.InputElement} {...props.elementConfig} />
