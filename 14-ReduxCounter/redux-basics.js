6339853791f2d7d60766ba2305c40d7ab0b3a875
AnandDev.Singh <ananddevsingh.aiet@gmail.com>
2019-05-27 18:18:53 +0530

    283. Dispatching Actions

--- 14-ReduxCounter/redux-basics.js
+++ 14-ReduxCounter/redux-basics.js
@@ -8,13 +8,28 @@ const initialState = {
 
 // Reducer
 const rootReducer = (state = initialState, action) => {
+  if (action.type === "INC_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + 1
+    };
+  }
+  if (action.type === "ADD_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + action.value
+    };
+  }
   return state;
 };
 
 // Store
 const store = createStore(rootReducer);
-console.log(store.getState());
+console.log(store.getState()); // It will return "initialState" becuase we don't have dispatched any action yet and not modified the store.
 
 // Dispatching Action
+store.dispatch({ type: "INC_COUNTER" });
+store.dispatch({ type: "ADD_COUNTER", value: 10 });
+console.log(store.getState()); // It will return "{ counter: 11 }".
 
 // Subscription
