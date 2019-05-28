6d624f0f2c02de2729b69ae3e33fd50827142e6f
AnandDev.Singh <ananddevsingh.aiet@gmail.com>
2019-05-29 01:28:11 +0530

    307. Adjusting Checkout and Contact Data

--- 08-BurgerBuilder/react-complete-guide/src/containers/Checkout/ContactData/ContactData.jsx
+++ 08-BurgerBuilder/react-complete-guide/src/containers/Checkout/ContactData/ContactData.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+
 import axios from "../../../axios-orders";
 
 import classes from "./ContactData.css";
@@ -105,7 +107,7 @@ class ContactData extends Component {
     }
 
     const order = {
-      ingredients: this.props.ingredients,
+      ingredients: this.props.ings,
       price: this.props.price,
       orderData: formData
     };
@@ -209,4 +211,11 @@ class ContactData extends Component {
   }
 }
 
-export default ContactData;
+const mapStateToProps = state => {
+  return {
+    ings: state.ingredients,
+    price: state.totalPrice
+  };
+};
+
+export default connect(mapStateToProps)(ContactData);
