2cf3ab08136d6d709a34e0c2a1f5d10dab24adb7
AnandDev.Singh <ananddevsingh.aiet@gmail.com>
2019-05-31 00:28:51 +0530

    319. Using Action Creators and Get State

--- 14-ReduxCounter/src/store/actions/message.js
+++ 14-ReduxCounter/src/store/actions/message.js
@@ -10,6 +10,7 @@ export const getName = name => {
 export const greet = name => {
   // Middleware runs between dispatching an action and the point of time action reaches the reducer.
   // Here dispatch will be available becuase we have added thunk.
+  // NOTE: Also, it gives second argument as "getState" function, that we can use to get the previous state of the store.
   return dispatch => {
     setTimeout(() => {
       dispatch(getName(name));
