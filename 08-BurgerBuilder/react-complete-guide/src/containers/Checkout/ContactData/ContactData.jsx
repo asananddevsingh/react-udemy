e11c15caa39d6fae6c751102326a7c735eb77dab
AnandDev.Singh <ananddevsingh.aiet@gmail.com>
2019-06-02 00:58:28 +0530

    335. Connecting Contact Data Container & Actions

--- 08-BurgerBuilder/react-complete-guide/src/containers/Checkout/ContactData/ContactData.jsx
+++ 08-BurgerBuilder/react-complete-guide/src/containers/Checkout/ContactData/ContactData.jsx
@@ -7,6 +7,8 @@ import classes from "./ContactData.css";
 import Button from "../../../components/UI/Button/Button";
 import Spinner from "../../../components/UI/Spinner/Spinner";
 import Input from "../../../components/UI/Input/Input";
+import withErrorHandler from "../../../hoc/withErrorHandler/withErrorHandler";
+import * as actions from "../../../store/actions";
 
 class ContactData extends Component {
   state = {
@@ -97,7 +99,6 @@ class ContactData extends Component {
 
   orderHandler = event => {
     event.preventDefault();
-    this.setState({ loading: true });
 
     const formData = {};
     for (const formElementIdentifier in this.state.orderForm) {
@@ -111,15 +112,8 @@ class ContactData extends Component {
       price: this.props.price,
       orderData: formData
     };
-    axios
-      .post("/orders.json", order)
-      .then(response => {
-        this.setState({ loading: false });
-        this.props.history.push("/");
-      })
-      .catch(error => {
-        this.setState({ loading: false });
-      });
+
+    this.props.onOrderBurger(order);
   };
 
   checkValidity(value, rules) {
@@ -218,4 +212,13 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps)(ContactData);
+const mapDispatchToProps = dispatch => {
+  return {
+    onOrderBurger: orderData => dispatch(actions.purchaseBurgerStart(orderData))
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withErrorHandler(ContactData, axios));
--- 08-BurgerBuilder/react-complete-guide/src/store/actions/index.js
+++ 08-BurgerBuilder/react-complete-guide/src/store/actions/index.js
@@ -3,3 +3,5 @@ export {
   addIngredient,
   removeIngredient
 } from "./burgerBuilder";
+
+export { purchaseBurgerStart } from "./order";
