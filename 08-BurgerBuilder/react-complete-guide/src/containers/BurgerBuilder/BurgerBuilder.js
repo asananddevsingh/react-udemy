6b74d7511b9a093ef161d35dfe1d381fb00513c6
AnandDev.Singh <ananddevsingh.aiet@gmail.com>
2019-06-12 00:52:45 +0530

    387. Testing Containers

--- 08-BurgerBuilder/react-complete-guide/src/containers/BurgerBuilder/BurgerBuilder.js
+++ 08-BurgerBuilder/react-complete-guide/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -11,7 +11,7 @@ import Spinner from "../../components/UI/Spinner/Spinner";
 import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 import * as actions from "../../store/actions";
 
-class BurgerBuilder extends React.Component {
+export class BurgerBuilder extends React.Component {
   state = {
     purchasing: false
   };
